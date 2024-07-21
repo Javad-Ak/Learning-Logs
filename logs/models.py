c13d29fb14f50ca724bf44a5fdadb78303c19b7e
Mohammad Javad Akbari <mojav.akbari@gmail.com>
2024-07-21 11:32:31 +0330

    initial commit

---
+++ logs/models.py
@@ -0,0 +1,29 @@
+"""Logs models: Topic and Entry"""
+from django.db import models
+
+
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
